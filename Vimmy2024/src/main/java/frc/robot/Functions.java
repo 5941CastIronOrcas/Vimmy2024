0ef746c9588d15d0582cac74ec08ef1c8cecdb3b
Gr3enie <137031040+Gr3enie@users.noreply.github.com>
2024-01-19 17:15:31 -0800

    Added some functions from Simmy

--- Vimmy2024/src/main/java/frc/robot/Functions.java
+++ Vimmy2024/src/main/java/frc/robot/Functions.java
@@ -1,6 +1,8 @@
 package frc.robot;
 import java.lang.Math;
 
+import edu.wpi.first.wpilibj.DriverStation;
+
 public class Functions {
     
     public static double DeadZone(double input, double deadZone) {
@@ -16,13 +18,15 @@ public class Functions {
     public static double Exponential(double input) {
         return input * Math.abs(input);
     }
-    public static double Pythagorean(double x, double y)
-    {
+    public static double Pythagorean(double x, double y) {
         return Math.sqrt((x * x) + (y * y));
     }
     public static double DeltaAngleDeg(double startAngle, double endAngle) {
         return((((endAngle - startAngle - 180) % 360) +360) % 360) - 180;
-      }
+    }
+    public static double DeltaAngleRadians(double startAngle, double endAngle) {
+        return ((((endAngle - startAngle - Math.PI) % (2*Math.PI)) + (2*Math.PI)) % (2*Math.PI))-Math.PI;
+    }
     public static double Clamp (double in, double min, double max) {
         return Math.min(Math.max(in,min),max);
     }
@@ -38,5 +42,22 @@ public class Functions {
             max = (values[i]>max)?values[i]:max;
         }
         return max;
+    }
+    public static double DriverToFieldAngle(double angle)
+    {
+        if(DriverStation.getAlliance().equals(DriverStation.Alliance.Red))
+        {angle = Functions.DeltaAngleDeg(0, angle - 90);}
+        else if(DriverStation.getAlliance().equals(DriverStation.Alliance.Blue))
+        {angle = Functions.DeltaAngleDeg(0, angle + 90);}
+        return angle;
+    }
+
+    public static double FieldToDriverAngle(double angle)
+    {
+        if(DriverStation.getAlliance().equals(DriverStation.Alliance.Red))
+        {angle = Functions.DeltaAngleDeg(0, angle + 90);}
+        else if(DriverStation.getAlliance().equals(DriverStation.Alliance.Blue))
+        {angle = Functions.DeltaAngleDeg(0, angle - 90);}
+        return angle;
     }
 }
