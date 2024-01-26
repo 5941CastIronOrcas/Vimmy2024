f53fbed82d5cdabe6c2f79a7180286527458f57f
gplastic <notanamev2@gmail.com>
2024-01-25 18:06:21 -0800

    ton of swerve fixing nonsense

--- Vimmy2024/src/main/java/frc/robot/Constants.java
+++ Vimmy2024/src/main/java/frc/robot/Constants.java
@@ -24,17 +24,17 @@ public final class Constants {
   //public static final Pigeon2 gyro = new Pigeon2(2);
   public static final Pigeon2 gyro = new Pigeon2(2);
 
-  public static final CANSparkMax flaMotor = new CANSparkMax(23, MotorType.kBrushless);
-  public static final CANSparkMax fltMotor = new CANSparkMax(22, MotorType.kBrushless);
-  public static final CANSparkMax fraMotor = new CANSparkMax(21, MotorType.kBrushless);
-  public static final CANSparkMax frtMotor = new CANSparkMax(20, MotorType.kBrushless);
-  public static final CANSparkMax blaMotor = new CANSparkMax(25, MotorType.kBrushless);
-  public static final CANSparkMax bltMotor = new CANSparkMax(24, MotorType.kBrushless);
-  public static final CANSparkMax braMotor = new CANSparkMax(27, MotorType.kBrushless);
-  public static final CANSparkMax brtMotor = new CANSparkMax(26, MotorType.kBrushless);
+  public static final CANSparkMax flaMotor = new CANSparkMax(27, MotorType.kBrushless);
+  public static final CANSparkMax fltMotor = new CANSparkMax(26, MotorType.kBrushless);
+  public static final CANSparkMax fraMotor = new CANSparkMax(25, MotorType.kBrushless);
+  public static final CANSparkMax frtMotor = new CANSparkMax(24, MotorType.kBrushless);
+  public static final CANSparkMax blaMotor = new CANSparkMax(21, MotorType.kBrushless);
+  public static final CANSparkMax bltMotor = new CANSparkMax(20, MotorType.kBrushless);
+  public static final CANSparkMax braMotor = new CANSparkMax(23, MotorType.kBrushless);
+  public static final CANSparkMax brtMotor = new CANSparkMax(22, MotorType.kBrushless);
 
-  public static final CANSparkMax armMotor1 = new CANSparkMax(0, MotorType.kBrushless);
-  public static final CANSparkMax armMotor2 = new CANSparkMax(0, MotorType.kBrushless);
+  public static final CANSparkMax armMotor1 = new CANSparkMax(28, MotorType.kBrushless);
+  public static final CANSparkMax armMotor2 = new CANSparkMax(29, MotorType.kBrushless);
   public static final Boolean armMotor1Invert = false;
   public static final Boolean armMotor2Invert = false;
 
@@ -51,13 +51,13 @@ public final class Constants {
   public static final double swerveDriveRatio = 1.00 / 6.75;
   public static final double swerveWheelCircumference = 0.096774 * Math.PI; // in m
   //public static final WPI_CANCoder flEncoder = new WPI_CANCoder(0);
-  public static final CANcoder flEncoder = new CANcoder(0);
-  public static final CANcoder frEncoder = new CANcoder(0);
-  public static final CANcoder blEncoder = new CANcoder(0);
-  public static final CANcoder brEncoder = new CANcoder(0);
+  public static final CANcoder flEncoder = new CANcoder(50);
+  public static final CANcoder frEncoder = new CANcoder(51);
+  public static final CANcoder blEncoder = new CANcoder(52);
+  public static final CANcoder brEncoder = new CANcoder(53);
 
   public static final double controllerDeadZone = 0.1;
-  public static final double modulePMult = 0.02;
+  public static final double modulePMult = 0.01;
   public static final double turnMult = 0.2;
   public static final double maxThrottleChange = 0.1;
   
