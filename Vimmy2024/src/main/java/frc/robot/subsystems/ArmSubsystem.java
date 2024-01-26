e67051c412b0b4d63d9313d897f6973b84fb7d00
Zerochan2800 <148506449+Zerochan2800@users.noreply.github.com>
2024-01-26 13:43:58 -0800

    Arm

---
+++ Vimmy2024/src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -0,0 +1,36 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Functions;
+
+public class ArmSubsystem extends SubsystemBase {
+    public double armAngle;
+  /** Creates a new ExampleSubsystem. */
+  public ArmSubsystem() {}
+
+  @Override
+  public void periodic() {
+    armAngle = Constants.armMotor1.getEncoder().getPosition() * 360 * Constants.armGearRatio;
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+
+  public void moveArmTo(double a1, double a2) {
+    rotateArm(Functions.Clamp((Constants.armMotorPMult * (a1 - armAngle)) 
+    +(Constants.armMotorGravMult) * Math.cos(Math.toRadians(armAngle)) 
+    +(Constants.armMotorDMult*Constants.armMotor1.getEncoder().getVelocity()), 
+    -Constants.maxArmSpeed, Constants.maxArmSpeed));
+    SmartDashboard.putNumber("Arm Motor Target", a1);
+  }
+  public void rotateArm(double t1) {
+    Constants.armMotor1.set((Constants.armMotor1Invert)?-t1:t1);
+    SmartDashboard.putNumber("Arm Motor Throttle", (Constants.armMotor1Invert)?-t1:t1);
+  }
+}
