98d0e23e8a35dc08ae728e40b330406996e89c4a
Gr3enie <137031040+Gr3enie@users.noreply.github.com>
2024-02-08 15:04:22 -0800

    Added list of all obstacles

--- Vimmy2024/src/main/java/frc/robot/subsystems/GOADataSynthesizer.java
+++ Vimmy2024/src/main/java/frc/robot/subsystems/GOADataSynthesizer.java
@@ -12,36 +12,31 @@ import frc.robot.utilityObjects.Obstacle;
 
 public class GOADataSynthesizer extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public static ArrayList<ArrayList<Obstacle>> staticObstacles = new ArrayList<ArrayList<Obstacle>>();
-  public static ArrayList<ArrayList<Obstacle>> movingObstacles = new ArrayList<ArrayList<Obstacle>>();
+  public static ArrayList<Obstacle> staticObstacles = new ArrayList<Obstacle>();
+  public static ArrayList<Obstacle> movingObstacles = new ArrayList<Obstacle>();
+  public static ArrayList<Obstacle> allObstacles = new ArrayList<Obstacle>();
   public GOADataSynthesizer() {
     //Red subwoofer
-    ArrayList<Obstacle> redSubwoofer = new ArrayList<Obstacle>(); 
-    redSubwoofer.add(new Obstacle(16.54-0.918718, 6.068568, true, Constants.subwooferAvoidanceMult));
-    redSubwoofer.add(new Obstacle(16.54-0, 6.53796, true, Constants.subwooferAvoidanceMult));
-    redSubwoofer.add(new Obstacle(16.54-0, 4.557776, true, Constants.subwooferAvoidanceMult));
-    redSubwoofer.add(new Obstacle(16.54-0.918718, 5.027168, true, Constants.subwooferAvoidanceMult));
+    staticObstacles.add(new Obstacle(16.54-0.918718, 6.068568, true, Constants.subwooferAvoidanceMult));
+    staticObstacles.add(new Obstacle(16.54-0, 6.53796, true, Constants.subwooferAvoidanceMult));
+    staticObstacles.add(new Obstacle(16.54-0, 4.557776, true, Constants.subwooferAvoidanceMult));
+    staticObstacles.add(new Obstacle(16.54-0.918718, 5.027168, true, Constants.subwooferAvoidanceMult));
     //Blue subwoofer
-    ArrayList<Obstacle> blueSubwoofer = new ArrayList<Obstacle>(); 
-    blueSubwoofer.add(new Obstacle(0.918718, 6.068568, true, Constants.subwooferAvoidanceMult));
-    blueSubwoofer.add(new Obstacle(0, 6.53796, true, Constants.subwooferAvoidanceMult));
-    blueSubwoofer.add(new Obstacle(0, 4.557776, true, Constants.subwooferAvoidanceMult));
-    blueSubwoofer.add(new Obstacle(0.918718, 5.027168, true, Constants.subwooferAvoidanceMult));
+    staticObstacles.add(new Obstacle(0.918718, 6.068568, true, Constants.subwooferAvoidanceMult));
+    staticObstacles.add(new Obstacle(0, 6.53796, true, Constants.subwooferAvoidanceMult));
+    staticObstacles.add(new Obstacle(0, 4.557776, true, Constants.subwooferAvoidanceMult));
+    staticObstacles.add(new Obstacle(0.918718, 5.027168, true, Constants.subwooferAvoidanceMult));
     //Red stage
-    ArrayList<Obstacle> redStage = new ArrayList<Obstacle>(); 
-    redStage.add(new Obstacle(13.4666, 4.105, true, Constants.subwooferAvoidanceMult));
-    redStage.add(new Obstacle(10.769374, 2.547726, true, Constants.subwooferAvoidanceMult));
-    redStage.add(new Obstacle(10.769374, 5.662274, true, Constants.subwooferAvoidanceMult));
-    //Blue stage
-    ArrayList<Obstacle> blueStage = new ArrayList<Obstacle>(); 
-    blueStage.add(new Obstacle(3.0734, 4.105, true, Constants.subwooferAvoidanceMult));
-    blueStage.add(new Obstacle(5.770626, 2.547726, true, Constants.subwooferAvoidanceMult));
-    blueStage.add(new Obstacle(5.770626, 5.662274, true, Constants.subwooferAvoidanceMult));
+    staticObstacles.add(new Obstacle(13.4666, 4.105, true, Constants.subwooferAvoidanceMult));
+    staticObstacles.add(new Obstacle(10.769374, 2.547726, true, Constants.subwooferAvoidanceMult));
+    staticObstacles.add(new Obstacle(10.769374, 5.662274, true, Constants.subwooferAvoidanceMult));
+    //Blue stage 
+    staticObstacles.add(new Obstacle(3.0734, 4.105, true, Constants.subwooferAvoidanceMult));
+    staticObstacles.add(new Obstacle(5.770626, 2.547726, true, Constants.subwooferAvoidanceMult));
+    staticObstacles.add(new Obstacle(5.770626, 5.662274, true, Constants.subwooferAvoidanceMult));
 
-    staticObstacles.add(redSubwoofer);
-    staticObstacles.add(blueSubwoofer);
-    staticObstacles.add(redStage);
-    staticObstacles.add(blueStage);
+    for (int i = 0; i < staticObstacles.size(); i++) allObstacles.add(staticObstacles.get(i));
+    for (int i = 0; i < movingObstacles.size(); i++) allObstacles.add(movingObstacles.get(i));
   }
 
   @Override
