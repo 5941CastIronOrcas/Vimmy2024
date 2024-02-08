d237af906573c35e8446248f4e4759469c67b1c6
DmytroHoloiadov <149747568+DmytroHoloiadov@users.noreply.github.com>
2024-02-07 16:41:41 -0800

    Arduino communication (full version)
    The fuctions are available, just implement in main code

--- Vimmy2024/src/main/java/frc/robot/subsystems/ArduinoCommunication.java
+++ Vimmy2024/src/main/java/frc/robot/subsystems/ArduinoCommunication.java
@@ -5,7 +5,7 @@ import frc.robot.Constants;
 
 public class ArduinoCommunication {
     // this function wraps arduino to port.
-    public void wrap() {
+    public static void Wrap() {
         try {
             Constants.arduino = new SerialPort(500000, SerialPort.Port.kUSB);
             System.out.println("Connected on kUSB");
@@ -22,5 +22,22 @@ public class ArduinoCommunication {
                 }
             }
         }
+    }
+    // this function will recall double value from [adressToPing]
+    // uncomment commented code below to have debug messages.
+    public static double RecallOneValue(byte adressToPing) {
+        double finalValue = -1;
+        Constants.arduino.write(new byte[] {adressToPing}, 1);
+        if (Constants.arduino.getBytesReceived() > 0) {
+            //System.out.println("Handling comminication");
+            String readed = Constants.arduino.readString();
+            //System.out.println("Recieved date succesfull. Data: " + readed);
+            try {
+                finalValue = Double.valueOf(readed);
+            } catch (Exception e) {
+                System.out.println(" \u001b[31;1m.The communication is out of sync!!!");
+            }
+        }
+        return finalValue;
     }
 }
