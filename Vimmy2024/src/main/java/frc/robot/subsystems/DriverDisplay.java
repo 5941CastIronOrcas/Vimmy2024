75b75f24195312909425f1799b6d8fd30e36efb6
Falsehook4716 <david445545@outlook.com>
2024-02-14 01:05:43 -0800

    added systems to robot container

--- Vimmy2024/src/main/java/frc/robot/subsystems/DriverDisplay.java
+++ Vimmy2024/src/main/java/frc/robot/subsystems/DriverDisplay.java
@@ -9,8 +9,7 @@ import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
-import frc.robot.Functions;
+
 
 
 public class DriverDisplay extends SubsystemBase {
