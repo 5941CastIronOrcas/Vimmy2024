93775de5d343cf1d3d1a9be896bf79a3ab4c0b0e
DmytroHoloiadov <149747568+DmytroHoloiadov@users.noreply.github.com>
2024-02-16 22:00:53 -0800

    Update GOADataSynthesizer.java

--- Vimmy2024/src/main/java/frc/robot/subsystems/GOADataSynthesizer.java
+++ Vimmy2024/src/main/java/frc/robot/subsystems/GOADataSynthesizer.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.utilityObjects.Obstacle;
 import frc.robot.utilityObjects.SonarModule;
+import frc.robot.utilityObjects.Vector2D;
 
 public class GOADataSynthesizer extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
@@ -31,9 +32,8 @@ public class GOADataSynthesizer extends SubsystemBase {
     //Blue stage 
     ,new Obstacle(3.0734, 4.105, true, Constants.stageAvoidanceMult, 0)
     ,new Obstacle(5.770626, 2.547726, true, Constants.stageAvoidanceMult, 0)
-    ,new Obstacle(5.770626, 5.662274, true, Constants.stageAvoidanceMult, 0)
-  };
-  public static ArrayList<Obstacle> allObstacles = new ArrayList<Obstacle>();
+    ,new Obstacle(5.770626, 5.662274, true, Constants.stageAvoidanceMult, 0)};
+  public static Obstacle[] allObstacles = new Obstacle[(int)(Constants.maxObstacleLife * 50 * 8 + staticObstacles.length)];
   public static SonarModule[] SonarModules = new SonarModule[] {
      new SonarModule(Constants.ultrasonicSensor1, Constants.servo1, 0, 0, 0)
     ,new SonarModule(Constants.ultrasonicSensor2, Constants.servo2, 0, 0, 0)
@@ -43,20 +43,18 @@ public class GOADataSynthesizer extends SubsystemBase {
     ,new SonarModule(Constants.ultrasonicSensor6, Constants.servo4, 0, 0, 0)
     ,new SonarModule(Constants.ultrasonicSensor7, Constants.servo4, 0, 0, 0)
     ,new SonarModule(Constants.ultrasonicSensor8, Constants.servo4, 0, 0, 0)};
-   // i don't know what's the error here
+    
+    Vector2D[] tempSensorReturn = new Vector2D[8];
+
    
   public GOADataSynthesizer() {
-    //Red subwoofer
-    for(int i = 0; i < 1; i++)
-    {
-      
-    }
- 
+    for (int i = 0; i < staticObstacles.length; i++) allObstacles[i] = new Obstacle(0, i, false, i, i); // merge the static into all
+    
   }
 
   @Override
   public void periodic() {
-    upTimeFrames++;
+    for (int i = 0; i < 8; i++) tempSensorReturn[i] = SonarModules[i].GetObstaclePosition(); // get readings from sensors
     
   }
 
