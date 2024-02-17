dfc9716e4082485e43a72008d8dcfc0a9bf70828
gplastic <notanamev2@gmail.com>
2024-02-17 14:46:17 -0800

    huh?
    -Dmytro

--- Vimmy2024/src/main/java/frc/robot/Robot.java
+++ Vimmy2024/src/main/java/frc/robot/Robot.java
@@ -118,6 +118,8 @@ public class Robot extends TimedRobot {
     double RSX = Functions.Exponential(Functions.DeadZone(Constants.controller1.getRightX(), Constants.controllerDeadZone)) * speed;
     double RSY = -Functions.Exponential(Functions.DeadZone(Constants.controller1.getRightY(), Constants.controllerDeadZone)) * speed;
     double LSY2 = -Functions.Exponential(Functions.DeadZone(Constants.controller2.getLeftY(), Constants.controllerDeadZone)) * speed;
+    double RSY2 = -Functions.Exponential(Functions.DeadZone(Constants.controller2.getRightY(), Constants.controllerDeadZone)) * speed;
+    double RSX2 = -Functions.Exponential(Functions.DeadZone(Constants.controller2.getRightX(), Constants.controllerDeadZone)) * speed;
     double RSAngle = 90-Math.toDegrees(Math.atan2(RSY, RSX));
 
     if (Constants.controller1.getRightBumperPressed()) {
@@ -159,8 +161,8 @@ public class Robot extends TimedRobot {
       ArmSubsystem.SpinIntake(0);
     }
     ArmSubsystem.SpinShooter(Constants.controller2.getRightTriggerAxis());
-    
-    ClimberSubsystem.moveClimbers(Constants.controller2.getRightY(), Constants.controller2.getRightX());
+
+    ClimberSubsystem.moveClimbers(RSY2, RSX2);
   }
 
 
