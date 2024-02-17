1f5fe01566ac6750d9110a76c9dcb84e83e58a9d
Falsehook4716 <david445545@outlook.com>
2024-02-16 18:00:48 -0800

    update photonvision

--- Vimmy2024/src/main/java/frc/robot/subsystems/DriverDisplay.java
+++ Vimmy2024/src/main/java/frc/robot/subsystems/DriverDisplay.java
@@ -111,8 +111,10 @@ public class DriverDisplay extends SubsystemBase {
     Vector2D nono = GOAGuidanceSystem.GetAvoidanceVector();
     DriverDisplay.avoidanceX.setDouble(nono.x);
     DriverDisplay.avoidanceY.setDouble(nono.y);
-    Pose2d positionPose2d = new Pose2d(PositionEstimator.robotPosition.getX(), PositionEstimator.robotPosition.getY(), new Rotation2d(-PositionEstimator.robotPosition.getRotation().getRadians() - Math.PI)); 
+    Pose2d positionPose2d = new Pose2d(PositionEstimator.robotPosition.getX(), PositionEstimator.robotPosition.getY(), new Rotation2d(-PositionEstimator.robotPosition.getRotation().getRadians() - Math.PI));
+    Pose2d block = new Pose2d(4, 3, new Rotation2d(1));
     m_field.setRobotPose(positionPose2d);
+    m_field.getObject("obj").setPose(block);
   
 
     //position estimator
