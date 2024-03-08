a26611f68f4157548feb0286c650fd9c816b01e3
creativit123 <ilikeairplanes134@gmail.com>
2024-03-08 13:11:03 -0800

    Update Constants.java

--- Vimmy2024/src/main/java/frc/robot/Constants.java
+++ Vimmy2024/src/main/java/frc/robot/Constants.java
@@ -198,8 +198,8 @@ public final class Constants {
   public static final double noteCameraForwardOffset = 0.54; // forward distance from robot center to note detector camera in meters
   public static final double noteCameraAngle = -10; //for note detector
   public static final double swerveMaxSpeed = 4.4; //the max speed we're capable of moving at in m/s (used for discarding impossible data)
-  public static final String apriltagCamera1Name = "Arducam_OV9281_USB_Camera 1";
-  public static final String apriltagCamera2Name = "Arducam_OV9281_USB_Camera 2";
+  public static final String apriltagCamera1Name = "Arducam_OV9281_USB_Camera 1"; //LEFT (shooter forward)
+  public static final String apriltagCamera2Name = "Arducam_OV9281_USB_Camera 2"; //RIGHT (shooter forward)
   public static final String noteDetectionCameraName = "HD_Web_Camera";
 
 
