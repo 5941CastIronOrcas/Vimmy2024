181ca4b684fe1f1331764b5aad1b9cc7244db539
Creativit123 <ilikeairplanes134@gmail.com>
2024-03-09 21:36:11 -0800

    small tweak

--- Vimmy2024/src/main/java/frc/robot/Robot.java
+++ Vimmy2024/src/main/java/frc/robot/Robot.java
@@ -257,7 +257,7 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousPeriodic() {
     
-    if(selectedAutoSequence >= 7)
+    if(selectedAutoSequence >= 7 && AutoSequences.isAutoTimeBetween(0.1, 16))
     {
       if (Math.abs(Functions.DeltaAngleDeg(PositionEstimator.angleToClosestNote(), PositionEstimator.robotPosition.getRotation().getDegrees())) < 15 && PositionEstimator.distToClosestNote() > 0.2+Constants.noteCameraForwardOffset && PositionEstimator.distToClosestNote() < 3+Constants.noteCameraForwardOffset && !NoteDetector.noteVisible) {
       framesNoteNotPresent++;
