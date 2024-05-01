1718f043bbfef8228403a3638513c94d070c0ca4
creativit123 <ilikeairplanes134@gmail.com>
2024-04-30 17:26:45 -0700

    ALL THE COMMENTS!!!
    most of these were done by Peder

--- Vimmy2024/src/main/java/frc/robot/subsystems/SwerveModule.java
+++ Vimmy2024/src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -18,7 +18,7 @@ public class SwerveModule {
     public double anglePos;
     //public double velocity;
     public double defaultAngle;
-    public SwerveModule (CANSparkMax angleMotorIN, CANSparkMax throttleMotorIN, CANcoder encoderIN, boolean aMotorInvert, boolean tMotorInvert, double dAngle) {
+    public SwerveModule (CANSparkMax angleMotorIN, CANSparkMax throttleMotorIN, CANcoder encoderIN, boolean aMotorInvert, boolean tMotorInvert, double dAngle) { //constructor used for each swerve module
         
         angleMotor = angleMotorIN;
         throttleMotor = throttleMotorIN;
@@ -28,33 +28,33 @@ public class SwerveModule {
         defaultAngle = dAngle;
         
     }
-    public double GetVelocity()
+    public double GetVelocity() //gets the velocity that the wheel is moving at in m/s
     {
         return tMult * Functions.DeadZone(((throttleMotor.getEncoder().getVelocity() + (encoder.getVelocity().getValueAsDouble()*(27.0 / 17.0)*(1.0/3.0)*60))/60.0) * Constants.swerveDriveRatio * Constants.swerveWheelCircumference, 0.00001);
     }
-    public double GetAngle()
+    public double GetAngle() //gets the angle of the wheel
     {
         return Functions.DeltaAngleDeg(0, encoder.getPosition().getValueAsDouble() * 360);
     }
-    public void Drive(double angle, double speed) {
+    public void Drive(double angle, double speed) { //turns the wheel to the correct angle, and spins the throttle motor.
         double throttle = speed;
         //anglePos = encoder.getAbsolutePosition().getValueAsDouble() * 360;
         
-        anglePos = Functions.DeltaAngleDeg(0, encoder.getPosition().getValueAsDouble() * 360);
+        anglePos = GetAngle();
         //velocity = tMult * Functions.DeadZone(((throttleMotor.getEncoder().getVelocity() + (encoder.getVelocity().getValueAsDouble()*(27.0 / 17.0)*(1.0/3.0)*60))/60.0) * Constants.swerveDriveRatio * Constants.swerveWheelCircumference, 0.00001);
 
-        if(Math.abs(speed) < 0.001) {
+        if(Math.abs(speed) < 0.001) { //if the target speed is slow enough, it rotates the wheels to make a diamond shape, improving traction when the robot isn't moving.
             //angle = anglePos;
             angle = defaultAngle;
         }
 
-        if (Math.abs(Functions.DeltaAngleDeg(anglePos, angle)) > Math.abs(Functions.DeltaAngleDeg(anglePos, angle + 180))) {
+        if (Math.abs(Functions.DeltaAngleDeg(anglePos, angle)) > Math.abs(Functions.DeltaAngleDeg(anglePos, angle + 180))) { //if the angle on the other side is closer, it just goes to that angle and rotates the wheel backwards.
             angle += 180;
             throttle *= -1;
         }
         //double compensationSpeed = -encoder.getVelocity().getValueAsDouble()*(27.0 / 17.0)*(1.0/3.0)*Constants.swerveDriveRatio*60;
         //double currentSpeed = throttleMotor.getEncoder().getVelocity();
-        angleMotor.set(Functions.Clamp(aMult*Functions.DeltaAngleDeg(angle, anglePos) * (Constants.modulePMult), -1,1));
+        angleMotor.set(Functions.Clamp(aMult*Functions.DeltaAngleDeg(angle, anglePos) * (Constants.modulePMult), -1,1)); //uses a P controller to rotate the wheel to the desired position.
        // throttleMotor.set(throttleMotor.get()+((compensationSpeed-currentSpeed)*0.00001));
         throttleMotor.set(throttle * tMult);
         
--- Vimmy2024/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ Vimmy2024/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -16,17 +16,17 @@ import java.lang.Math;
  **/
 
 public class SwerveSubsystem extends SubsystemBase {
-  public static double xOut = 0.0;
+  public static double xOut = 0.0; //x and y inputs with clamped acceleration
   public static double yOut = 0.0;
-  public static double flThrottleOut = 0.0;
+  public static double flThrottleOut = 0.0; //same as xOut and yOut but for throttle for each swerve module
   public static double frThrottleOut = 0.0;
   public static double blThrottleOut = 0.0;
   public static double brThrottleOut = 0.0;
-  public static double oldAngle = 0.0;
+  public static double oldAngle = 0.0; //used to find the rate of change of the angle to the speaker
   public static double newAngle = 0.0;
-  public static Vector2D goaAvoidVector = new Vector2D(0, 0);
-  public static double goaAvoidAngle = 0.0;
-  public static SwerveModule flModule = new SwerveModule(Constants.flaMotor,Constants.fltMotor,Constants.flEncoder,true,Constants.fltInvert,45);
+  public static Vector2D goaAvoidVector = new Vector2D(0, 0); //vector used to avoid obstacles
+  public static double goaAvoidAngle = 0.0; //used to point intake away from obstacles
+  public static SwerveModule flModule = new SwerveModule(Constants.flaMotor,Constants.fltMotor,Constants.flEncoder,true,Constants.fltInvert,45); //assigns each swerve module
   public static SwerveModule frModule = new SwerveModule(Constants.fraMotor,Constants.frtMotor,Constants.frEncoder,true,Constants.frtInvert,-45);
   public static SwerveModule blModule = new SwerveModule(Constants.blaMotor,Constants.bltMotor,Constants.blEncoder,true,Constants.bltInvert,-45);
   public static SwerveModule brModule = new SwerveModule(Constants.braMotor,Constants.brtMotor,Constants.brEncoder,true,Constants.frtInvert,45);
@@ -47,7 +47,7 @@ public class SwerveSubsystem extends SubsystemBase {
     // This method will be called once per scheduler run during simulation
   }
   
-  public static void DriveTo(double x, double y, double angle, double speedLimit, double turnLimit, double XOffset, double YOffset)
+  public static void DriveTo(double x, double y, double angle, double speedLimit, double turnLimit, double XOffset, double YOffset) //uses a PD controller and the Drive function to drive to a given point on the field.
   {
     /*double angleToTarget = 90-Math.atan2(y-PositionEstimator.robotPosition.getY(), x-PositionEstimator.robotPosition.getX());
     double pComponent = Constants.swerveDriveToPMult*Functions.Pythagorean(x-PositionEstimator.robotPosition.getX(), y-PositionEstimator.robotPosition.getY());
@@ -65,31 +65,31 @@ public class SwerveSubsystem extends SubsystemBase {
     double yComponent = output * Math.cos(angleToTarget);
     DriveFieldOrientedAtAngle(xComponent+(Robot.isRedAlliance?-YOffset:YOffset), yComponent+(Robot.isRedAlliance?XOffset:-XOffset), angle, turnLimit);
   }
-  public static void GOADriveTo(double x, double y, double angle, double speedLimit, double turnLimit, double XOffset, double YOffset)
+  public static void GOADriveTo(double x, double y, double angle, double speedLimit, double turnLimit, double XOffset, double YOffset) //same as DriveTo, but avoids obstacles with GOA.
   {
     Vector2D v = GOAGuidanceSystem.GetDriveVector(x, y, goaAvoidVector, speedLimit);
     DriveFieldOrientedAtAngle(v.x, v.y, angle, turnLimit);
   }
-  public static void GOADriveToProtectIntake (double x, double y, double speedLimit, double turnLimit, double XOffset, double YOffset) {
+  public static void GOADriveToProtectIntake (double x, double y, double speedLimit, double turnLimit, double XOffset, double YOffset) { //same as GOADriveTo, but rotates to protect the intake.
     GOADriveTo(x, y, goaAvoidAngle, speedLimit, turnLimit, XOffset, YOffset);
   }
   
-  public static void DriveFieldOriented(double x, double y, double turn)
+  public static void DriveFieldOriented(double x, double y, double turn) //same as DriveDriverOriented, but drives relative to the field instead of the driver.
   {
     DriveDriverOriented(Robot.isRedAlliance?y:-y, Robot.isRedAlliance?-x:x, turn);
   }
   
-  public static void DriveFieldOrientedAtAngle(double x, double y, double angle, double turnLimit)
+  public static void DriveFieldOrientedAtAngle(double x, double y, double angle, double turnLimit) //same as DriveDriverOrientedAtAngle, but drives relative to the field instead of the driver.
   {
     DriveDriverOrientedAtAngle(Robot.isRedAlliance?y:-y, Robot.isRedAlliance?-x:x, Functions.FieldToDriverAngle(angle), turnLimit);
   }
 
-  public static void DriveDriverOriented(double LSX, double LSY, double RSX)
+  public static void DriveDriverOriented(double LSX, double LSY, double RSX) //similar to Drive, but when the driver pushes up on the left stick, the robot moves forward from their point of view instead of the robot's.
   {
     Drive(LSX*Math.cos(Math.toRadians(-PositionEstimator.robotYawDriverRelative))+LSY*Math.sin(Math.toRadians(-PositionEstimator.robotYawDriverRelative)), LSY*Math.cos(Math.toRadians(-PositionEstimator.robotYawDriverRelative))+LSX*Math.sin(Math.toRadians(PositionEstimator.robotYawDriverRelative)), RSX);
   }
 
-  public static void DriveDriverOrientedAtAngle(double LSX, double LSY, double angle, double turnLimit)
+  public static void DriveDriverOrientedAtAngle(double LSX, double LSY, double angle, double turnLimit) //uses a PD controller to rotate the robot to a specific angle, instead of rotating the robot at a certain speed. otherwise, the same as DriveDriverOriented.
   {
     turnLimit = Functions.Clamp(turnLimit, 0, 1);
     double t = Functions.Clamp(-Constants.swerveAutoTurnPMult*Functions.DeadZone(
@@ -101,14 +101,14 @@ public class SwerveSubsystem extends SubsystemBase {
 
     DriveDriverOriented(LSX, LSY, t);
   }
-  public static void DriveDriverOrientedProtectIntake(double LSX, double LSY, double turnLimit) {
+  public static void DriveDriverOrientedProtectIntake(double LSX, double LSY, double turnLimit) { //same as DriveDriverOriented, but rotates to protect the intake.
     DriveDriverOrientedAtAngle(LSX, LSY, Functions.FieldToDriverAngle(goaAvoidAngle), turnLimit);
   }
-  public static void DriveToProtectIntake(double x, double y, double speedLimit, double turnLimit, double XOffset, double YOffset) {
+  public static void DriveToProtectIntake(double x, double y, double speedLimit, double turnLimit, double XOffset, double YOffset) { //same as DriveTo, but rotates to protect the intake.
     DriveTo(x, y, goaAvoidAngle, speedLimit, turnLimit, XOffset, YOffset);
   }
 
-  public static void FaceSpeaker(double x, double y, double turnLimit) {
+  public static void FaceSpeaker(double x, double y, double turnLimit) { //rotates the robot to face the speaker.
     //double aSpeed = (newAngle - oldAngle) * 50;
     //double a = PositionEstimator.angleToSpeaker() + (aSpeed * (PositionEstimator.distToSpeaker() / Constants.launchSpeed));
     //DriveDriverOrientedAtAngle(x, y, Functions.FieldToDriverAngle(a), turnLimit);
@@ -122,7 +122,7 @@ public class SwerveSubsystem extends SubsystemBase {
     DriveDriverOrientedAtAngle(x, y, Functions.FieldToDriverAngle(a), turnLimit);
   }
 
-  public static void SquareDriveTo(double x, double y, double angle, double speedLimit, double turnLimit, double XOffset, double YOffset, boolean xFirst) {
+  public static void SquareDriveTo(double x, double y, double angle, double speedLimit, double turnLimit, double XOffset, double YOffset, boolean xFirst) { //same as DriveTo, but moves on one axis before moving on the other.
     if (xFirst) {
       DriveTo(x, Math.abs(y-PositionEstimator.robotPosition.getY())<Constants.swerveSquareDriveToDeadZone?y:PositionEstimator.robotPosition.getY(), angle, speedLimit, turnLimit, XOffset, YOffset);
     }
@@ -131,13 +131,13 @@ public class SwerveSubsystem extends SubsystemBase {
     }
   }
 
-  public static void GoToAmp(double speedLimit, double turnLimit, double XOffset, double YOffset)
+  public static void GoToAmp(double speedLimit, double turnLimit, double XOffset, double YOffset) //drives to the amp.
   {
     Vector2D target = Robot.isRedAlliance?Constants.redAmpDepositPosition:Constants.blueAmpDepositPosition;
     DriveTo(target.x, PositionEstimator.robotPosition.getY(), 0, speedLimit, turnLimit, XOffset, YOffset);
   }
 
-  public static void CollectNote(double XOffset, double YOffset, double speedLimit) {
+  public static void CollectNote(double XOffset, double YOffset, double speedLimit) { //if the note is visible, it will drive toward it, and intake the note.
     /*if (NoteDetector.noteVisible && !ArmSubsystem.hasNote) {
       double a = PositionEstimator.robotYawDriverRelative + NoteDetector.noteYaw;
       double x = -Math.cos(Math.toRadians(-a+90)) * NoteDetector.noteDist + XOffset;
@@ -160,13 +160,13 @@ public class SwerveSubsystem extends SubsystemBase {
 
   }
   
-  public static void Drive(double x, double y, double rotate) {
+  public static void Drive(double x, double y, double rotate) { //this is the basis of the swerve code
     double currentMaxAccel = Constants.swerveMaxAccel;
     //uncomment the below line to enable adaptive acceleration limiter
     currentMaxAccel = Functions.Clamp(Constants.swerveMaxAccelExtended + (Math.cos(Math.toRadians(ArmSubsystem.armAngle))*(Constants.swerveMaxAccel-Constants.swerveMaxAccelExtended)), 0, 2.0);
-    xOut += Functions.Clamp(x-xOut, -currentMaxAccel, currentMaxAccel);
+    xOut += Functions.Clamp(x-xOut, -currentMaxAccel, currentMaxAccel); //xOut and yOut are x and y, but the acceleration is limited.
     yOut += Functions.Clamp(y-yOut, -currentMaxAccel, currentMaxAccel);
-    double flx =  xOut + (Constants.turnMult * rotate);
+    double flx =  xOut + (Constants.turnMult * rotate); //the x and y coordinates of each wheel. since the rotation affects each wheel differently, rotation is either added or subtracted from x and y.
     double fly =  yOut + (Constants.turnMult * rotate);
     double frx =  xOut + (Constants.turnMult * rotate);
     double fry =  yOut - (Constants.turnMult * rotate);
@@ -174,24 +174,24 @@ public class SwerveSubsystem extends SubsystemBase {
     double bly =  yOut + (Constants.turnMult * rotate);
     double brx =  xOut - (Constants.turnMult * rotate);
     double bry =  yOut - (Constants.turnMult * rotate);
-    double maxDist = Functions.Max(new double[]{
+    double maxDist = Functions.Max(new double[]{ //the max amplitude of the vectors made by the x and y coordinates. the minimum value is 1.
       Functions.Pythagorean(flx, fly),
       Functions.Pythagorean(frx, fry),
       Functions.Pythagorean(blx, bly),
-      Functions.Pythagorean(brx, bry),1.0}); //use x*x not Math.pow()
-    double flAngle = 90-Math.toDegrees(Math.atan2(fly,flx));
+      Functions.Pythagorean(brx, bry),1.0});
+    double flAngle = 90-Math.toDegrees(Math.atan2(fly,flx)); //the angle each module turns to.
     double frAngle = 90-Math.toDegrees(Math.atan2(fry,frx));
     double blAngle = 90-Math.toDegrees(Math.atan2(bly,blx));
     double brAngle = 90-Math.toDegrees(Math.atan2(bry,brx));
-    double flThrottle = Functions.Pythagorean(flx, fly) / maxDist;
+    double flThrottle = Functions.Pythagorean(flx, fly) / maxDist; //the power assigned to each throttle motor. each value is divided by the max, as to keep all values at or under 1.
     double frThrottle = Functions.Pythagorean(frx, fry) / maxDist;
     double blThrottle = Functions.Pythagorean(blx, bly) / maxDist;
-    double brThrottle = Functions.Pythagorean(brx, bry) / maxDist; //use x*x not Math.pow()
-    flThrottleOut += Functions.Clamp(flThrottle-flThrottleOut, -Constants.maxThrottleChange, Constants.maxThrottleChange);
+    double brThrottle = Functions.Pythagorean(brx, bry) / maxDist;
+    flThrottleOut += Functions.Clamp(flThrottle-flThrottleOut, -Constants.maxThrottleChange, Constants.maxThrottleChange); //same as xOut and yOut, but limiting the throttle instead.
     frThrottleOut += Functions.Clamp(frThrottle-frThrottleOut, -Constants.maxThrottleChange, Constants.maxThrottleChange);
     blThrottleOut += Functions.Clamp(blThrottle-blThrottleOut, -Constants.maxThrottleChange, Constants.maxThrottleChange);
     brThrottleOut += Functions.Clamp(brThrottle-brThrottleOut, -Constants.maxThrottleChange, Constants.maxThrottleChange);
-    flModule.Drive(flAngle, flThrottleOut);
+    flModule.Drive(flAngle, flThrottleOut); //telling each individual module to move.
     frModule.Drive(frAngle, frThrottleOut);
     blModule.Drive(blAngle, blThrottleOut);
     brModule.Drive(brAngle, brThrottleOut);
